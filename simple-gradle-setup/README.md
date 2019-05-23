85b312c80a59d861eecc036f28ca80203ace88f8
Jonathan Brighton <brightonsbox@hotmail.com>
2019-05-23 20:19:47 +0100

    Adding a simple Gradle Hello World project

---
+++ simple-gradle-setup/README.md
@@ -0,0 +1,6 @@
+# Simple Gradle Setup
+
+This is a Hello World project built with Gradle.
+
+To generate a runnable JAR file, please run ./gradlew clean shadowJar
+
---
+++ simple-gradle-setup/src/main/java/com/brightonsbox/Main.java
@@ -0,0 +1,12 @@
+package com.brightonsbox;
+
+public class Main {
+
+    public String getGreeting() {
+        return "Hello world.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Main().getGreeting());
+    }
+}
